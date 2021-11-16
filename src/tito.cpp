85b7f3add882c6be6b6d405e239c869fed6979e5
Danny Piper <djpiper28@gmail.com>
2021-11-16 18:34:39 +0000

    fix voided tickets
    when tickets are voided it goes tits up

--- src/tito.cpp
+++ src/tito.cpp
@@ -322,31 +322,31 @@ std::list<TitoAttendee> TitoApi::getAttendees()
         std::string name;
         attendee.at("name").get_to(name);
 
-        std::string email = "no_email";
-        if (!attendee.at("email").is_null()) {
+        std::string email, phoneNumber;
+        bool valid = attendee.at("email").is_null();
+        valid = !valid;
+        if (valid) {
             attendee.at("email").get_to(email);
-        }
-        std::string phoneNumber;
-        if (!attendee.at("phone_number").is_null()) {
-            attendee.at("phone_number").get_to(phoneNumber);
-        }
+            if (!attendee.at("phone_number").is_null()) {
+                attendee.at("phone_number").get_to(phoneNumber);
+            }
 
-        int ticketID;
-        attendee.at("id").get_to(ticketID);
-        std::string ticketSlug;
-        attendee.at("slug").get_to(ticketSlug);
-        std::string ticketRelease;
-        attendee.at("release_title").get_to(ticketRelease);
+            int ticketID;
+            attendee.at("id").get_to(ticketID);
+            std::string ticketSlug;
+            attendee.at("slug").get_to(ticketSlug);
+            std::string ticketRelease;
+            attendee.at("release_title").get_to(ticketRelease);
 
-        // The API is poor, so I have to ask for the check ins later
-        TitoTicket ticket(ticketID, ticketSlug, ticketRelease);
+            // The API is poor, so I have to ask for the check ins later
+            TitoTicket ticket(ticketID, ticketSlug, ticketRelease);
 
 #ifdef DEBUG
-        std::cerr << "Debug TitoApi::getAttendees() : Found an attendee with name " 
-                  << name << std::endl;
+            std::cerr << "Debug TitoApi::getAttendees() : Found an attendee with name "
+                    << name << std::endl;
 #endif
-                  
-        tmp.push_back(new TitoAttendee(name, email, phoneNumber, ticket));
+            tmp.push_back(new TitoAttendee(name, email, phoneNumber, ticket));
+        }
     }
     
     // Parse the checkins
